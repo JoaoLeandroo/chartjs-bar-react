00cb92d185f2a718af91c014c9011b951d789f8b
joaoleandro <new.joaoleandro@gmail.com>
2023-10-10 21:44:41 -0300

    grafico de barras

--- src/app/components/grafico/page.jsx
+++ src/app/components/grafico/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-
+import { useRef } from 'react'
 import {
     Chart as ChartJs,
     BarElement,
@@ -8,7 +8,7 @@ import {
     Tooltip,
     Legend
 } from 'chart.js'
-import { Bar, Chart } from 'react-chartjs-2'
+import { Bar, getElementAtEvent } from 'react-chartjs-2'
 
 ChartJs.register(
     BarElement,
@@ -18,6 +18,7 @@ ChartJs.register(
     Legend
 )
 
+
 export default function Grafico() {
     const mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"]
     const vendedor_1 = [23, 112, 104, 57, 98, 123]
@@ -25,7 +26,7 @@ export default function Grafico() {
     const vendedor_3 = [55, 44, 108, 110, 90, 101]
 
     const data = {
-        labels: ['mon', 'Tue', 'Wed', 'Thu'],
+        labels: mes,
         datasets: [{
             label: "Victor",
             data: vendedor_1,
@@ -49,13 +50,22 @@ export default function Grafico() {
     }
     const options = {}
 
-
+    const chartRef = useRef()
+    const onClick = (e) => {
+        if(getElementAtEvent(chartRef.current, e).length > 0) {
+            console.log(getElementAtEvent(chartRef.current, e) [0])
+            const dataSetIndex = getElementAtEvent(chartRef.current, e) [0].datasetIndex
+            console.log(dataSetIndex)
+        }
+    }
 
     return(
         <div className='max-w-[700px] w-full'>
             <Bar
                 data={data}
                 options={options}
+                onClick={onClick}
+                ref={chartRef}
             >
 
             </Bar>
